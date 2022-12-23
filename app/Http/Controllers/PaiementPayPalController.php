e7d93676b22d1c71c617a75522d5164b7af8e18f
motonim <motonimjrp@gmail.com>
2022-12-22 21:32:57 -0500

    configuration du Paypal API

--- app/Http/Controllers/PaiementPayPalController.php
+++ app/Http/Controllers/PaiementPayPalController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\PaiementPaypal;
 use Omnipay\Omnipay;
 
 class PaiementPayPalController extends Controller
@@ -12,8 +13,8 @@ class PaiementPayPalController extends Controller
     public function __construct()
     {
         $this->gateway = Omnipay::create('PayPal_Rest');
-        $this->gateway->setClientId('PAYPAL_CLIENT_ID');
-        $this->gateway->setSecret('PAYPAL_CLIENT_SECRET');
+        $this->gateway->setClientId(env('PAYPAL_CLIENT_ID'));
+        $this->gateway->setSecret(env('PAYPAL_CLIENT_SECRET'));
         $this->gateway->setTestMode(true);
     }
 
@@ -25,7 +26,7 @@ class PaiementPayPalController extends Controller
                 'currency' => env('PAYPAL_CURRENCY'),
                 'returnUrl' => url('success'),
                 'cancelUrl' => url('error')
-            ));
+            ))->send();
 
             if($response->isRedirect()) {
                 $response->redirect();
@@ -36,5 +37,43 @@ class PaiementPayPalController extends Controller
         } catch (\Throwable $th) {
             return $th->getMessage();
         }
+    }
+
+    public function success(Request $request) {
+        if($request->input('paymentId') && $request->input('PayerID')){
+            $transaction = $this->gateway->completePurchase(array(
+                'payer_id' => $request->input('PayerID'),
+                'transactionReference' => $request->input('paymentId')
+            ));
+
+            $response = $transaction->send();
+
+            if($response->isSuccessful()){
+                $arr = $response->getData();
+
+                $payment = new PaiementPaypal;
+                $payment->paiement_id = $arr['id'];
+                $payment->payeur_id = $arr['payer']['payer_info']['payer_id'];
+                $payment->payeur_courriel = $arr['payer']['payer_info']['email'];
+                $payment->prix = $arr['transactions'][0]['amount']['total'];
+                $payment->monnaie = env('PAYPAL_CURRENCY');
+                $payment->paiement_statut = $arr['state'];
+
+                $payment->save();
+
+                return "Le paiement a été effectué. L'ID transaction est : " . $arr['id'];
+            }
+            else {
+                return $response->getMessage();
+            }
+        }
+        else {
+            return 'Le paiement a été refusé.';
+        }
+
+    }
+
+    public function error() {
+        return "L'utilisateur a refusé le paiement.";
     }
 }
